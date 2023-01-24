d95567a7fc28fbfb7008f4e296e1a329803b2607
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-01-24 14:00:48 +0100

    Fix modal features

--- src/modal-features.jl
+++ src/modal-features.jl
@@ -12,14 +12,13 @@ export interpret_feature
 ############################################################################################
 ############################################################################################
 ############################################################################################
-const U = Real
 
 # A modal feature represents a function that can be computed on a world.
 # The simplest example is, min(A1), which computes the minimum for attribute 1
 #  for a given world.
 # The value of a feature for a given world can be then evaluated in a condition,
 #  such as: min(A1) >= 10.
-abstract type ModalFeature{U} <: AbstractFeature{U} end
+abstract type ModalFeature{U<:Real} <: AbstractFeature{U} end
 
 Base.show(io::IO, f::ModalFeature, args...; kwargs...) = print(io, display_feature(f, args...; kwargs...))
 
@@ -43,7 +42,7 @@ abstract type DimensionalFeature{U} <: ModalFeature{U} end
 struct MultiAttributeFeature{U} <: DimensionalFeature{U}
     f::Function
 end
-function interpret_feature(f::MultiAttributeFeature{U}, inst::AbstractDimensionalInstance{T})::U where {U,T}
+function interpret_feature(f::MultiAttributeFeature{U}, inst::AbstractDimensionalInstance{T})::U where {U<:Real,T}
     (f.f(inst))
 end
 display_feature(f::MultiAttributeFeature,         args...; kwargs...) = "$(f.f)"
@@ -74,28 +73,32 @@ display_feature(f::SingleAttributeNamedFeature;    attribute_names_map::Union{No
 #  e.g., min(A1), max(A10)
 struct SingleAttributeMin{U} <: SingleAttributeFeature{U}
     i_attribute::Integer
-    function SingleAttributeMin{U}(i_attribute::Integer)
+    function SingleAttributeMin{U}(i_attribute::Integer) where {U<:Real}
         return new{U}(i_attribute)
     end
     function SingleAttributeMin(i_attribute::Integer)
+        @warn "Please specify the type of the feature for SingleAttributeMin." *
+            " For example: SingleAttributeMin{Float64}($(i_attribute))."
         return SingleAttributeMin{Real}(i_attribute)
     end
 end
-function interpret_feature(f::SingleAttributeMin{U}, inst::AbstractDimensionalInstance{T}) where {U,T}
+function interpret_feature(f::SingleAttributeMin{U}, inst::AbstractDimensionalInstance{T}) where {U<:Real,T}
     (minimum(get_instance_attribute(inst,f.i_attribute)))::U
 end
 display_feature(f::SingleAttributeMin;             attribute_names_map::Union{Nothing,AbstractVector,AbstractDict} = nothing) = "min($(attribute_name(f; attribute_names_map = attribute_names_map)))"
 
 struct SingleAttributeMax{U} <: SingleAttributeFeature{U}
     i_attribute::Integer
-    function SingleAttributeMax{U}(i_attribute::Integer)
+    function SingleAttributeMax{U}(i_attribute::Integer) where {U<:Real}
         return new{U}(i_attribute)
     end
     function SingleAttributeMax(i_attribute::Integer)
+        @warn "Please specify the type of the feature for SingleAttributeMax." *
+            " For example: SingleAttributeMax{Float64}($(i_attribute))."
         return SingleAttributeMax{Real}(i_attribute)
     end
 end
-function interpret_feature(f::SingleAttributeMax{U}, inst::AbstractDimensionalInstance{T}) where {U,T}
+function interpret_feature(f::SingleAttributeMax{U}, inst::AbstractDimensionalInstance{T}) where {U<:Real,T}
     (maximum(get_instance_attribute(inst,f.i_attribute)))::U
 end
 display_feature(f::SingleAttributeMax;             attribute_names_map::Union{Nothing,AbstractVector,AbstractDict} = nothing) = "max($(attribute_name(f; attribute_names_map = attribute_names_map)))"
@@ -107,7 +110,7 @@ display_feature(f::SingleAttributeMax;             attribute_names_map::Union{No
 struct SingleAttributeSoftMin{U,T<:AbstractFloat} <: SingleAttributeFeature{U}
     i_attribute::Integer
     alpha::T
-    function SingleAttributeSoftMin{U}(i_attribute::Integer, alpha::T) where {T}
+    function SingleAttributeSoftMin{U}(i_attribute::Integer, alpha::T) where {U<:Real,T}
         @assert !(alpha > 1.0 || alpha < 0.0) "Can't instantiate SingleAttributeSoftMin with alpha = $(alpha)"
         @assert !isone(alpha) "Can't instantiate SingleAttributeSoftMin with alpha = $(alpha). Use SingleAttributeMin instead!"
         new{U,T}(i_attribute, alpha)
@@ -116,19 +119,19 @@ end
 alpha(f::SingleAttributeSoftMin) = f.alpha
 display_feature(f::SingleAttributeSoftMin;         attribute_names_map::Union{Nothing,AbstractVector,AbstractDict} = nothing) = "min" * util.subscriptnumber(rstrip(rstrip(string(f.alpha*100), '0'), '.')) * "($(attribute_name(f; attribute_names_map = attribute_names_map)))"
 
-function interpret_feature(f::SingleAttributeSoftMin{U}, inst::AbstractDimensionalInstance{T}) where {U,T}
+function interpret_feature(f::SingleAttributeSoftMin{U}, inst::AbstractDimensionalInstance{T}) where {U<:Real,T}
     util.softminimum(get_instance_attribute(inst,f.i_attribute), f.alpha)::U
 end
 struct SingleAttributeSoftMax{U,T<:AbstractFloat} <: SingleAttributeFeature{U}
     i_attribute::Integer
     alpha::T
-    function SingleAttributeSoftMax{U}(i_attribute::Integer, alpha::T) where {T}
+    function SingleAttributeSoftMax{U}(i_attribute::Integer, alpha::T) where {U<:Real,T}
         @assert !(alpha > 1.0 || alpha < 0.0) "Can't instantiate SingleAttributeSoftMax with alpha = $(alpha)"
         @assert !isone(alpha) "Can't instantiate SingleAttributeSoftMax with alpha = $(alpha). Use SingleAttributeMax instead!"
         new{U,T}(i_attribute, alpha)
     end
 end
-function interpret_feature(f::SingleAttributeSoftMax{U}, inst::AbstractDimensionalInstance{T}) where {U,T}
+function interpret_feature(f::SingleAttributeSoftMax{U}, inst::AbstractDimensionalInstance{T}) where {U<:Real,T}
     util.softmaximum(get_instance_attribute(inst,f.i_attribute), f.alpha)::U
 end
 alpha(f::SingleAttributeSoftMax) = f.alpha
@@ -151,7 +154,7 @@ struct SingleAttributeGenericFeature{U} <: SingleAttributeFeature{U}
     i_attribute::Integer
     f::Function
 end
-function interpret_feature(f::SingleAttributeGenericFeature{U}, inst::AbstractDimensionalInstance{T}) where {U,T}
+function interpret_feature(f::SingleAttributeGenericFeature{U}, inst::AbstractDimensionalInstance{T}) where {U<:Real,T}
     (f.f(util.vectorize(get_instance_attribute(inst,f.i_attribute));))::U
 end
 display_feature(f::SingleAttributeGenericFeature;  attribute_names_map::Union{Nothing,AbstractVector,AbstractDict} = nothing) = "$(f.f)($(attribute_name(f; attribute_names_map = attribute_names_map)))"
