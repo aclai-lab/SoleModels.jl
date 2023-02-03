347694b8e8fb1eef7739e625756a8821433e4fe1
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-02-03 10:42:46 +0100

    Minor

--- src/conditional-data/full-dimensional-frame/Full1DFrame+RCC.jl
+++ src/conditional-data/full-dimensional-frame/Full1DFrame+RCC.jl
@@ -7,12 +7,12 @@ computeModalThresholdDual(test_operator::CanonicalFeatureGeq, w::Interval, r::RC
         map((IA_r)->(yieldReprs(test_operator, enum_acc_repr(test_operator, w, IA_r, length(channel)), channel)), topo2IARelations(r))
     )
 end
-compute_modal_gamma(test_operator::CanonicalFeatureGeq, w::Interval, r::RCC8RelationFromIA, channel::DimensionalChannel{T,1}) where {T} = begin
+apply_aggregator(test_operator::CanonicalFeatureGeq, w::Interval, r::RCC8RelationFromIA, channel::DimensionalChannel{T,1}) where {T} = begin
     maximum(
         map((IA_r)->(yieldRepr(test_operator, enum_acc_repr(test_operator, w, IA_r, length(channel)), channel)), topo2IARelations(r))
     )
 end
-compute_modal_gamma(test_operator::CanonicalFeatureLeq, w::Interval, r::RCC8RelationFromIA, channel::DimensionalChannel{T,1}) where {T} = begin
+apply_aggregator(test_operator::CanonicalFeatureLeq, w::Interval, r::RCC8RelationFromIA, channel::DimensionalChannel{T,1}) where {T} = begin
     mininimum(
         map((IA_r)->(yieldRepr(test_operator, enum_acc_repr(test_operator, w, IA_r, length(channel)), channel)), topo2IARelations(r))
     )
@@ -26,12 +26,12 @@ computeModalThresholdDual(test_operator::CanonicalFeatureGeq, w::Interval, r::RC
         map((IA_r)->(yieldReprs(test_operator, enum_acc_repr(test_operator, w, IA_r, size(channel)...), channel)), [IA_r for RCC8_r in RCC52RCC8Relations(r) for IA_r in topo2IARelations(RCC8_r)])
     )
 end
-compute_modal_gamma(test_operator::CanonicalFeatureGeq, w::Interval, r::RCC5Relation, channel::DimensionalChannel{T,1}) where {T} = begin
+apply_aggregator(test_operator::CanonicalFeatureGeq, w::Interval, r::RCC5Relation, channel::DimensionalChannel{T,1}) where {T} = begin
     maximum(
         map((IA_r)->(yieldRepr(test_operator, enum_acc_repr(test_operator, w, IA_r, size(channel)...), channel)), [IA_r for RCC8_r in RCC52RCC8Relations(r) for IA_r in topo2IARelations(RCC8_r)])
     )
 end
-compute_modal_gamma(test_operator::CanonicalFeatureLeq, w::Interval, r::RCC5Relation, channel::DimensionalChannel{T,1}) where {T} = begin
+apply_aggregator(test_operator::CanonicalFeatureLeq, w::Interval, r::RCC5Relation, channel::DimensionalChannel{T,1}) where {T} = begin
     mininimum(
         map((IA_r)->(yieldRepr(test_operator, enum_acc_repr(test_operator, w, IA_r, size(channel)...), channel)), [IA_r for RCC8_r in RCC52RCC8Relations(r) for IA_r in topo2IARelations(RCC8_r)])
     )
