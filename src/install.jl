a1d720b19f18ff556c5093b5b5d6cc54d9533dd3
mauro-milella <milella_mauro@live.it>
2023-07-05 17:07:18 +0200

    Minor changes involving installer

--- src/install.jl
+++ src/install.jl
@@ -6,7 +6,7 @@
 using Pkg
 
 # Remove the specified package (do not abort if it is already removed) and reinstall it.
-function install(package::String, url::String, rev::String)
+function install(package::String, url::String, rev::Union{Nothing,String} = nothing)
     printstyled(stdout, "\nRemoving: $package\n", color=:green)
     try
         Pkg.rm(package)
@@ -16,15 +16,21 @@ function install(package::String, url::String, rev::String)
 
     printstyled(stdout, "\nFetching: $url at branch $rev\n", color=:green)
     try
-        Pkg.add(url=url, rev=rev)
+        if !isnothing(rev)
+            Pkg.add(url=url, rev=rev)
+        else
+            Pkg.add(url=url)
+        end
+
         printstyled(stdout, "\nPackage $package instantiated correctly\n", color=:green)
     catch error
         println(); showerror(stdout, error); println()
     end
 end
 
-install("SoleBase", "https://github.com/aclai-lab/SoleBase.jl", "dev")
-install("SoleData", "https://github.com/aclai-lab/SoleData.jl", "dev")
-install("SoleLogics", "https://github.com/aclai-lab/SoleLogics.jl", "dev-v0.9.1")
+install("SoleBase", "https://github.com/aclai-lab/SoleBase.jl", "dev-v0.9.1")
+install("SoleData", "https://github.com/aclai-lab/SoleData.jl", "dev-v0.9.1")
+# install("SoleLogics", "https://github.com/aclai-lab/SoleLogics.jl", "dev-v0.9.1")
+install("SoleLogics", "../SoleLogics.jl")
 
 Pkg.instantiate()
