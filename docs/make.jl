8580cfabc37e4466849c5ddf13015842f134801b
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-11-11 15:02:24 +1100

    Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,5 @@
+using SoleBase
+using SoleLogics
 using SoleModels
 using Documenter
 
@@ -8,9 +10,10 @@ DocMeta.setdocmeta!(SoleModels, :DocTestSetup, :(using SoleModels); recursive=tr
 makedocs(;
     modules=[SoleBase, SoleLogics, SoleModels],
     authors="Michele Ghiotti, Giovanni Pagliarini, Eduard I. Stan",
-    repo="https://github.com/aclai-lab/SoleModels.jl/blob/{commit}{path}#{line}",
+    repo=Documenter.Remotes.GitHub("aclai-lab", "SoleModels.jl"),
     sitename="SoleModels.jl",
     format=Documenter.HTML(;
+        size_threshold = 4000000,
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://aclai-lab.github.io/SoleModels.jl",
         assets=String[],
