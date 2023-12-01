09dc95db67131c52bc822b4c9e2945d097e2c17c
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-12-01 17:52:52 +1100

    Fix

--- src/experimentals.jl
+++ src/experimentals.jl
@@ -4,7 +4,7 @@ export formula2natlang
 
 using SoleModels
 using SoleLogics
-import SoleLogics: formula2natlang
+import SoleLogics.experimentals: formula2natlang
 
 function formula2natlang(φ::MultiFormula; kwargs...)
     @assert length(modforms(φ)) == 1 "Cannot apply formula2natlang to MultiFormula with $(length(modforms(φ))) modforms"
