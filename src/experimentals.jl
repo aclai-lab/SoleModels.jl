47defac3b913d78ab9ef19e0eca30f88287b79e8
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-12-07 10:28:15 +1100

    Fix

--- src/experimentals.jl
+++ src/experimentals.jl
@@ -8,10 +8,11 @@ import SoleLogics.experimentals: formula2natlang
 
 function formula2natlang(
     f::MultiFormula;
-    hidemodality = false,
+    hidemodality = nothing,
     variable_names_map::Union{Nothing,AbstractDict,AbstractVector,AbstractVector{<:Union{AbstractDict,AbstractVector}}} = nothing,
     kwargs...
 )
+    isnothing(hidemodality) && (hidemodality = (length(modforms(f)) == 1)) # TODO if variable_names_map isa Vector of strings, then do not complain is hidemodality is nothing
     map_is_multimodal = begin
         if !isnothing(variable_names_map) && all(e->!(e isa Union{AbstractDict,AbstractVector}), variable_names_map)
             (haskey(kwargs, :silent) && kwargs[:silent]) ||
@@ -28,15 +29,4 @@ function formula2natlang(
         φ = formula2natlang(modforms(f)[i_modality]; variable_names_map = _variable_names_map, kwargs...)
         hidemodality ? "$φ" : "On modality $(i_modality): $φ"
     end for i_modality in sort(collect(keys(modforms(f))))], "\n\tAND\n")
-end
-    for (i_modality, φ) in pairs(modforms(φ))
-        if hidemodality
-            println(" $i_modality: $(formula2natlang(φ; kwargs...))\n")
-        else
-            @assert length(modforms(φ)) == 1 "Cannot apply formula2natlang to MultiFormula with $(length(modforms(φ))) modforms"
-            println(formula2natlang(φ; kwargs...))
-        end
-    end
-end
-
 end
