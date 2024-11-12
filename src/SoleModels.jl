90f12dc9890526a8988472abb3c6be2b8ebdec4c
mauro-milella <milella_mauro@live.it>
2024-11-12 18:23:17 +0100

    dispatches from `symbolic-utils.jl` to `types` and `utils/models`

--- src/SoleModels.jl
+++ src/SoleModels.jl
@@ -31,6 +31,7 @@ using SoleData.DimensionalDatasets: globalrel
 export AbstractModel
 export iscomplete
 export outcometype, outputtype
+export immediatesubmodels, nimmediatesubmodels, listimmediaterules
 export apply, apply!
 export info, info!, hasinfo
 export wrap
@@ -79,7 +80,9 @@ export printmodel, displaymodel
 
 include("print.jl")
 
-export immediatesubmodels, listimmediaterules
+export submodels, nsubmodels
+export leafmodels, nleafmodels
+export subtreeheight
 export listrules, joinrules
 
 include("symbolic-utils.jl")
