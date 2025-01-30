4b9563a0f01865785da613acfdaba6f0420c6c89
PasoStudio73 <paso.studio73@gmail.com>
2025-01-30 17:59:03 +0100

    modified get_condition function

--- ext/DecisionTreeExt.jl
+++ ext/DecisionTreeExt.jl
@@ -9,7 +9,8 @@ import DecisionTree as DT
 function get_condition(featid, featval, featurenames)
     test_operator = (<)
     # @show fieldnames(typeof(tree))
-    feature = !isnothing(featurenames) ? VariableValue(featurenames[featid]) : VariableValue(featid)
+    # feature = !isnothing(featurenames) ? VariableValue(featurenames[featid]) : VariableValue(featid)
+    feature = !isnothing(featurenames) ? VariableValue(featurenames[featid], featid) : VariableValue(featid)
     return ScalarCondition(feature, test_operator, featval)
 end
 
