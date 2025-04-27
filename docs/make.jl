3c2984f0b7cd6b20d67d6e9b1906fffaec58e0bd
giopaglia <24519853+giopaglia@users.noreply.github.com>
2025-04-27 14:08:56 +0200

    Fix

--- docs/make.jl
+++ docs/make.jl
@@ -35,7 +35,7 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/aclai-lab/SoleModels.jl",
-    devbranch = "dev",
+    devbranch = "main",
     target = "build",
     branch = "gh-pages",
     versions = ["main" => "main", "stable" => "v^", "v#.#", "dev" => "dev"],
