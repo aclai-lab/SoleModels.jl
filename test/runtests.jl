bd9918745e1587a3ace9865b4b32a96cb53668e0
PasoStudio73 <paso.studio73@gmail.com>
2025-07-21 19:57:23 +0200

    temporary commented pluto demo

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,7 +42,8 @@ test_suites = [
     ("Parse", ["parse.jl", ]),
     ("Rules", ["juliacon2024.jl", ]),
     ("Linear forms", ["linear-form-utilities.jl", ]),
-    ("Pluto Demo", ["$(dirname(dirname(pathof(SoleModels))))/pluto-demo.jl", ]),
+    # timeseries site closed, look for local dataset in SoleData asap
+    # ("Pluto Demo", ["$(dirname(dirname(pathof(SoleModels))))/pluto-demo.jl", ]),
     ("DecisionTreeExt", ["DecisionTreeExt/tree.jl", "DecisionTreeExt/forest.jl", "DecisionTreeExt/adaboost.jl"]),
     ("XGBoostExt", ["XGBoostExt/xgboost_classifier.jl", "XGBoostExt/xgboost_regressor.jl"]),
 ]
